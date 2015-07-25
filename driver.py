3a73ca68eda79bc3f1f3bff53e4ce3995e8b533f
CRImier <crimier@yandex.ru>
2015-07-25 14:34:41 +0300

    Modified callback mechanism - now it's either uinput or external callback, if the latter, uinput instance is not created

--- driver.py
+++ driver.py
@@ -23,15 +23,15 @@ class Chatpad():
     #Key dictionary
     key_dict = {17: 'KEY_7', 18: 'KEY_6', 19: 'KEY_5', 20: 'KEY_4', 21: 'KEY_3', 22: 'KEY_2', 23: 'KEY_1', 33: 'KEY_U', 34: 'KEY_Y', 35: 'KEY_T', 36: 'KEY_R', 37: 'KEY_E', 38: 'KEY_W', 39: 'KEY_Q', 49: 'KEY_J', 50: 'KEY_H', 51: 'KEY_G', 52: 'KEY_F', 53: 'KEY_D', 54: 'KEY_S', 55: 'KEY_A', 65: 'KEY_N', 66: 'KEY_B', 67: 'KEY_V', 68: 'KEY_C', 69: 'KEY_X', 70: 'KEY_Z', 81: 'KEY_RIGHT', 82: 'KEY_M', 83: 'KEY_DOT', 84: 'KEY_SPACE', 85: 'KEY_LEFT', 98: 'KEY_COMMA', 99: 'KEY_ENTER', 100: 'KEY_P', 101: 'KEY_0', 102: 'KEY_9', 103: 'KEY_8', 113: 'KEY_BACKSPACE', 114: 'KEY_L', 117: 'KEY_O', 118: 'KEY_I', 119: 'KEY_K'}
 
-
     def __init__(self, port = "/dev/ttyAMA0", name="xbox_chatpad_input", callback = None):
-        if callback == None:
-            callback = lambda *args: None #Empty callback
         self.callback = callback
+        if callback == None: #If we don't have an external callback provided, we should be using a built-in one
+            self.callback = self.uinput_callback
         self.name = name
         self.port = port
         self.serial = Serial(port, 19200)
-        if name: self.uinput = UInput(name=name, devnode='/dev/uinput')
+        if name or callback == None: 
+            self.uinput = UInput(name=name, devnode='/dev/uinput')
         
     def send_init_message(self):
         #print "Init message sent"
@@ -44,6 +44,15 @@ class Chatpad():
     def test_chatpad_on_serial(self):
         # TODO: Add Chatpad detection on a serial port (using 5-byte init response messages starting with A5)
         return NotImplementedError
+
+    def uinput_callback(pressed, released):
+        for key in pressed: 
+            #print "Key pressed: "+self.key_dict[key]
+            self.uinput.write(ecodes.EV_KEY, ecodes.ecodes[self.key_dict[key]], 1)
+        for key in released:
+            #print "Key released: "+self.key_dict[key]
+            self.uinput.write(ecodes.EV_KEY, ecodes.ecodes[self.key_dict[key]], 0)
+        self.uinput.syn()
 
     def thread_listen(self):
         self.send_init_message()
@@ -88,18 +97,10 @@ class Chatpad():
                     continue #Duplicate message received, no need to worry
                 #Next 9 lines would be a perfect example of where to use sets - wonder what is their performance and if that would be premature optimisation
                 pressed = [key for key in pressed_keys if key not in previous_keys]
-                for key in pressed: 
-                    print "Key pressed: "+self.key_dict[key]
-                    self.uinput.write(ecodes.EV_KEY, ecodes.ecodes[self.key_dict[key]], 1)
                 released = [key for key in previous_keys if key not in pressed_keys]
-                for key in released:
-                    print "Key released: "+self.key_dict[key]
-                    self.uinput.write(ecodes.EV_KEY, ecodes.ecodes[self.key_dict[key]], 0)
-                held = [key for key in previous_keys if key in pressed_keys]
-                for key in held:
-                    print "Key held: "+self.key_dict[key]
-                self.uinput.syn()
-                self.callback(pressed, released, held)
+                #Keys read, time to process
+                self.callback(pressed, released) 
+                #All done.
                 previous_keys = pressed_keys
                 #if modifiers: print "Modifiers are: "+str(modifiers)
             sleep(1.0/counter_treshold)
